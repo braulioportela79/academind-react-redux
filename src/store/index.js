5733987ac62f41cb454eb72864210fed3bececc3
braulioportela79 <braulioportela@outlook.com>
2022-05-01 13:02:07 -0300

    Creating Store

---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+import { createStore } from 'react';
+
+const counterReducer = (state = { counter: 0 }, action) => {
+    if (action.type === 'increment') {
+        return {
+            counter: state.counter + 1
+        }
+    }
+
+    if (action.type === 'increment') {
+        return {
+            counter: state.counter - 1
+        }
+    }
+
+    return state;
+};
+
+const store = createStore(counterReducer);
+
+export default store;
